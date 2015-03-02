f1b90946fe7ce2db12dc6effc2eece154ba29f7e
Luu Hoang Nam <hoangnam0705@icloud.com>
2015-03-02 08:24:08 +0700

    Add migration

---
+++ database/migrations/2015_03_02_012237_create_slugs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateSlugsTable
+ */
+class CreateSlugsTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slugs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+
+            $table->unsignedInteger('sluggable_id');
+            $table->string('sluggable_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('slugs');
+    }
+
+}
